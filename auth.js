d5272c3fd1538c4276a86a7aa52873b59c57d502
vimal raj <163636182+vimal2512@users.noreply.github.com>
2025-02-25 22:24:11 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,30 +1,10 @@
-//checks if user is authenticated or not
+const express = require('express')
+const router = express.Router()
 
-const User = require('../models/user')
-const jwt=require("jsonwebtoken")
-const ErrorHandler = require("../utils/errorhandler");
-const catchAsyncError = require("./catchAsyncError");
+const{registerUser,loginUser,logout}=require('../controllers/authController')
 
-exports.isAuthenticatedUser = catchAsyncError(async(req,res,next)=>{
-  const {token}= req.cookies
-  if(!token){
-    return next(new ErrorHandler('Login first to access the resource',401))
-  }
-  const decoded = jwt.verify(token,process.env.JWT_SECRET)
-  req.user=await User.findById(decoded.id)
-  next()
-})
+router.route('/register').post(registerUser)
+router.route('/login').post(loginUser)
+router.route('/logout').get(logout)
 
-//Handling users roles 
-
-exports.authorizeRoles=(...roles)=>{
-  return(req,res,next)=>{
-    if(!roles.includes(req.user.role)){
-      return next(
-        new ErrorHandler(`Role ${req.user.role} is not allowed to access the resource`,403)
-      )
-     
-    }
-    next()
-  }
-}
+module.exports=router
--- product.js
+++ product.js
@@ -1,92 +1,15 @@
-const mongoose = require('mongoose')
-const productSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"please enter product name"],
-        trim:true,
-        maxLength:[100,"product name cannot exceed 100 characters"]
-    },
-    price:{
-        type:Number,
-        required:[true,"please enter product price"],
-        maxLength:[5,"product name cannot exceed 5 characters"],
-        default:0.0
-    },
-    description:{
-        type:String,
-        required:[true,"please enter product description"],
-    },
-    ratings:{
-        type:Number,
-        default:0
-    },
-    images:[
-        {
-            public_id:{
-                type:String,
-                required:true
-            },
-            url:{
-                type:String,
-                required:true
-            }
-        }
-    ],
-    category:{
-        type:String,
-        required:[true,'please select category for this product'],
-        enum:{
-            values:[
-                'Electronics',
-                'Camera',
-                'Laptop',
-                'Accessories',
-                'Headphones',
-                'Food',
-                'Books',
-                'Clothes/Shoes',
-                'Beauty/Health',
-                'Sports',
-                'Outdoor',
-                'Home'
-            ],
-            message:"please select correct category for products"
-        }
-    },
-    seller:{
-        type:String,
-        required:[true,'please enter product seller']
-    },
-    stock:{
-        type:Number,
-        required:[true,"please enter product stock"],
-        maxLength:[5,'product name cannot exceed 5 characters'],
-        default:0
-    },
-    numOfReviews:{
-        type:Number,
-        default:0
-    },
-    reviews:[
-        {
-            name:{
-                type:String,
-                required:true
-            },
-            rating:{
-                type:Number,
-                required:true
-            },
-            comment:{
-                type:String,
-                required:true
-            }
-        }
-    ],
-    createdAt:{
-        type:Date,
-        default:Date.now
-    }
-})
+const express = require('express')
+const router = express.Router()
 
-module.exports = mongoose.model('Product',productSchema)
+const {getProducts,newProduct,getSingleProduct,updateProduct,deleteProduct}=require('../controllers/productController')
+const{isAuthenticatedUser,authorizeRoles}=require('../middlewares/auth')
+
+
+
+router.route('/products').get(getProducts)
+
+router.route('/product/:id').get(getSingleProduct)
+router.route('/admin/product/new').post(isAuthenticatedUser,authorizeRoles('admin'),newProduct)
+router.route('/admin/product/:id').put(isAuthenticatedUser,authorizeRoles('admin'),updateProduct).delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProduct)
+
+module.exports = router
